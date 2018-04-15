1112a757f85fc8529d14a9c55d5dd8b950900ca7
Paul Carroll <p.v.carroll@gmail.com>
2018-04-15 09:36:02 -0500

    explore tab bar

--- PulseOfAustin/Explore/ExploreViewController.swift
+++ PulseOfAustin/Explore/ExploreViewController.swift
@@ -11,3 +11,17 @@ import UIKit
 class ExploreViewController: UIViewController {
     
 }
+
+extension ExploreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ExploreViewController: UITableViewDelegate {
+    
+}
