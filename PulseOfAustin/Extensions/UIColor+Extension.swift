cf7f8afc5a6d9bc0ed190927e06c9b2d0e8ace37
Paul Carroll <p.v.carroll@gmail.com>
2018-04-19 21:35:01 -0500

    colors, add TopicIntroVC, UI work

--- PulseOfAustin/Extensions/UIColor+Extension.swift
+++ PulseOfAustin/Extensions/UIColor+Extension.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 extension UIColor {
+    
+    public class var customYellow: UIColor {
+        return UIColor(red:0.97, green:0.70, blue:0.07, alpha:1.0)
+    }
+    public class var blueAccent: UIColor {
+        return UIColor(red:0.61, green:0.84, blue:0.83, alpha:1.0)
+    }
+    public class var customOrange: UIColor {
+        return UIColor(red:0.92, green:0.44, blue:0.01, alpha:1.0)
+    }
+    public class var whiteText: UIColor {
+        return UIColor(red:0.98, green:0.98, blue:0.98, alpha:1.0)
+    }
+    public class var whiteBackground: UIColor {
+        return UIColor(red:0.98, green:0.98, blue:0.98, alpha:1.0)
+    }
+    public class var basicsBarBlue: UIColor {
+        return UIColor(red:0.72, green:0.82, blue:0.90, alpha:1.0)
+    }
     public class var customDarkText: UIColor {
         return UIColor(red:0.29, green:0.29, blue:0.29, alpha:1.0)
+    }
+    public class var infoCardBackground: UIColor {
+        return UIColor(red:0.94, green:0.93, blue:0.91, alpha:1.0)
     }
 }
---
+++ PulseOfAustin/Topic/TopicIntroViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TopicIntroViewController.swift
+//  PulseOfAustin
+//
+//  Created by Paul Carroll on 4/19/18.
+//  Copyright © 2018 Paul Carroll. All rights reserved.
+//
+
+import UIKit
+
+class TopicIntroViewController: UIViewController {
+    
+}
