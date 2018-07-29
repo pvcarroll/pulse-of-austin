80b67e90e073145286060505a903ba2942b1cfe9
Paul Carroll <p.v.carroll@gmail.com>
2018-07-29 12:55:58 -0500

    Learn map displays Austin

--- PulseOfAustin/Topic/Learn/LearnMap.swift
+++ PulseOfAustin/Topic/Learn/LearnMap.swift
@@ -12,4 +12,13 @@ import MapKit
 class LearnMap: UIView {
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        mapView.showsUserLocation = true
+        
+        let center = CLLocationCoordinate2DMake(30.26, -97.7)
+        let span = MKCoordinateSpanMake(0.2, 0.3)
+        mapView.region = MKCoordinateRegionMake(center, span)
+    }
 }
