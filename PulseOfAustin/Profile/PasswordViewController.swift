03e4364457658fa3765354ddbe74df3b9e1b01ee
Paul Carroll <p.v.carroll@gmail.com>
2018-09-23 12:39:30 -0500

    Profile: change password screen

---
+++ PulseOfAustin/Profile/PasswordViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PasswordViewController.swift
+//  PulseOfAustin
+//
+//  Created by Paul Carroll on 9/23/18.
+//  Copyright © 2018 Paul Carroll. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+//        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+//        
+//        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.dismissKeyboardOnMainViewTap()
+        self.confirmButton.addBottomShadow()
+        let forgotPasswordAttributedTitle: NSMutableAttributedString = NSMutableAttributedString(string: "Forgot Password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.darkGray67_62_54])
+        forgotPasswordAttributedTitle.addUnderline()
+        self.forgotPasswordButton.setAttributedTitle(forgotPasswordAttributedTitle, for: .normal)
+        
+        // Adjust scroll view when keyboard is visible
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+            self.scrollView.contentInset = UIEdgeInsets.zero
+        } else {
+            self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        self.scrollView.scrollIndicatorInsets = self.scrollView.contentInset
+    }
+}
